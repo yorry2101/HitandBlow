c86d330bde17f5d38e5dcbc32748aefa6a564655
yorry2101 <yorry2101@outlook.jp>
2025-12-23 16:01:25 +0900

    グラフィックインターフェースに切り替え

--- hit_and_blow_app.py
+++ hit_and_blow_app.py
@@ -1,6 +1,9 @@
 # hit and blow_app.py
-from text_interface import TextInterface
+#from text_interface import TextInterface
+from game_manager import GameManager
+from graphic_interface import GraphicInterface
 if __name__ == "__main__":
     answer = "1234"  # ここでは固定の答えを使用。実際にはランダム生成などが望ましい。
-    interface = TextInterface(answer)
+    #interface = TextInterface(answer)
+    interface = GraphicInterface(answer)
     interface.start()
