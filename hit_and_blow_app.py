a239aa4f48fad8f83afc4a674ca444cd35efed3f
yorry2101 <yorry2101@outlook.jp>
2025-12-23 16:05:24 +0900

    game_managerにコメントを追加，appの正解ランダム化を再度反映

--- hit_and_blow_app.py
+++ hit_and_blow_app.py
@@ -2,8 +2,17 @@
 #from text_interface import TextInterface
 from game_manager import GameManager
 from graphic_interface import GraphicInterface
+import random
+
 if __name__ == "__main__":
-    answer = "1234"  # ここでは固定の答えを使用。実際にはランダム生成などが望ましい。
+    answer = ""
+    used = []
+
+    while len(answer) < 4:
+        d = random.randint(0, 9)
+        if d not in used:
+            used.append(d)
+            answer += str(d)  # ここでは固定の答えを使用。実際にはランダム生成などが望ましい。
     #interface = TextInterface(answer)
     interface = GraphicInterface(answer)
     interface.start()
